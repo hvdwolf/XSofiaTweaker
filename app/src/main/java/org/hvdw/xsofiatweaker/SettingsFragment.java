05dbf61bc6bf13e80b405c3efdac2c8f4562b690
hvdwolf <hvdwolf@gmail.com>
2018-03-14 18:14:26 +0100

    correct tab; update version

--- app/src/main/java/org/hvdw/xsofiatweaker/SettingsFragment.java
+++ app/src/main/java/org/hvdw/xsofiatweaker/SettingsFragment.java
@@ -239,7 +239,7 @@ public class SettingsFragment extends PreferenceFragment implements OnSharedPref
             case MySettings.PREF_DISABLE_BTPHONETOP:
                 intent.setAction(MySettings.ACTION_PREF_DISABLE_BTPHONETOP_CHANGED);
                 intent.putExtra(MySettings.EXTRA_PREF_DISABLE_BTPHONETOP_ENABLED, sharedPreferences.getBoolean(key, false));
-        toastText = "BOOLEAN_KEY";
+        	toastText = "BOOLEAN_KEY";
                 break;
             default:
                 Log.d(TAG, "Invalid setting encountered");
